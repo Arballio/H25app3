a8fae43c4afcc52d07b01aca98b35ff6bfd0972a
Arballio <arthur.blanchard022@gmail.com>
2025-02-04 18:16:41 -0500

    Donnée importé et balbutiment des historigrammes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 def check_arg(args=None):
     parser = argparse.ArgumentParser(description='Script to learn basic argparse')
@@ -13,6 +13,15 @@ def check_arg(args=None):
     results = parser.parse_args(args)
     return results.name
 
+
+dataPrim = np.genfromtxt('Detecteur_Primaire.csv', delimiter=',')
+
+dataSec = np.genfromtxt('Detecteur_Secondaire.csv', delimiter=',')
+
 if __name__ == '__main__':
     n = check_arg(sys.argv[1:])
     print('n =',n)
+
+
+    plt.hist(dataPrim[:,1], bins=100)
+    plt.show()
