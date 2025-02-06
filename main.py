ff6b84ee2e0185f94a8e06fa0b60c08624955e6f
Karl <walk6522@usherbrooke.ca>
2025-02-05 22:18:49 -0500

    Working func

--- main.py
+++ main.py
@@ -21,15 +21,75 @@ def check_arg(args=None):
 
     return parser.parse_args(args)
 
+
+def Coincident(Array1, Array2):
+    roundedArray1 = set(np.around(Array1[:, 1], 0))
+    roundedArray2 = set(np.around(Array2[:, 1], 0))
+    #roundedArray1 = set(Array1[:,1])
+    #roundedArray2 = set(Array2[:, 1])
+
+    #roundedArray1 = list(roundedArray1)
+    temp = roundedArray1.intersection(roundedArray2)
+    temp = sorted(temp)
+    temp = list(temp)
+    roundedArray1 = sorted(roundedArray1)
+    roundedArray1 = list(roundedArray1)
+    indexTemp = 0
+    length = len(temp)
+    resultat = [0]*len(roundedArray1)
+    resultat2 = [0]*len(roundedArray1)
+    for i in range(len(roundedArray1)):
+        j = indexTemp
+        while j < length:
+            if roundedArray1[i] == temp[j]:
+                resultat[i] = Array1[i,2]
+                indexTemp = j
+                break
+
+            j += 1
+
+        if length == j:
+            resultat2[i] = Array1[i,2]
+
+
+
+    return resultat, resultat2
+
+def nonCoincident(Array1, Array2):
+    roundedArray1 = set(np.around(Array1[:, 1], 1))
+    roundedArray2 = set(np.around(Array2[:, 1], 1))
+
+    #roundedArray1 = list(roundedArray1)
+    temp = roundedArray1.intersection(roundedArray2)
+    temp = sorted(temp)
+    temp = list(temp)
+    roundedArray1 = sorted(roundedArray1)
+    roundedArray1 = list(roundedArray1)
+
+    length = len(temp)
+    resultat = [0]*len(roundedArray1)
+
+    for i in range(len(roundedArray1)):
+        for j in range(len(temp)):
+            if roundedArray1[i] != temp[j]:
+                resultat[i] = Array1[i,2]
+                #break
+
+    return resultat
+
 def main():
     args = check_arg()
 
     data_prim = np.genfromtxt(args.nameprim, delimiter=',')
     data_sec = np.genfromtxt(args.namesec, delimiter=',')
 
-    # print('np =',args.nameprim,'ns =',args.namesec)
+    Coincide,NonCoincide = Coincident(data_prim, data_sec)
+    Coincide = sorted(Coincide)
+    print(Coincide)
+    #NonCoincident = nonCoincident(data_prim, data_sec)
+    '''# print('np =',args.nameprim,'ns =',args.namesec)
 
-    Attend = sorted(dataPrim[:,2])
+    Attend = sorted(data_prim[:,2])
     setAttend = sorted(set(Attend))
     listAttend = list(setAttend)
 
@@ -39,24 +99,22 @@ def main():
     temp = [0]*lenSetAttend
 
     j = 0
-    for i in range(lenSetAttend):
-        while listAttend[i] == Attend[j]:
-            temp[i] += 1
-            j += 1
-            if  j >= len(Attend):
-                break;
 
 
-    #print(temp)
 
 
-    #print(myList)
-
+    #print('Z = ',z)
+'''
+    #print(NonCoincide)
+   # print(data_prim[:,2])
     plt.ylabel('Rate')
     plt.xlabel('Amplitude (mV)')
     plt.semilogx()
     plt.title('Amplitude lue selon le temps')
-    plt.hist(temp, bins=np.logspace(1,2, num = 50 ), histtype='step' )
+    plt.grid()
+    plt.hist(Coincide[:], bins=np.logspace(1, 3, num=20), histtype='step', color = 'RED')
+    plt.hist(NonCoincide[:], bins=np.logspace(1, 3, num=20), histtype='step', color = 'GREEN')
+    plt.hist(data_prim[:,2], bins=np.logspace(1,3, num = 20 ), histtype='step', color = 'BLUE' )
     plt.show()
 
 
